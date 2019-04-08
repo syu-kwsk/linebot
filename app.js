a722e5ceb514d311fd3f46ba3751bc8dcf1d8f5e
Your Name <you@example.com>
2019-04-09 00:19:03 +0900

    try

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ server.post("/", line.middleware(lineConfig), (req, res) => {
   // LINEのサーバーに200を返す
   res.sendStatus(200);
 
-  const message = createReplyMessage(req.body.events[0].source.message);
-  lineClient.replyMessage(req.body.events[0].replyToken, message);
+  const message = createReplyMessage(req.body.events[0].source);
+  lineClient.replyMessage(req.body.events[0].replyToken, source);
   
 });
 
