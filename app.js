d2ac818afcea2ac050812095018f8b7083543f80
Your Name <you@example.com>
2019-04-08 23:58:13 +0900

    try

--- app.js
+++ app.js
@@ -9,38 +9,25 @@ const lineConfig = {
 const lineClient = new line.Client(lineConfig);
 
 function createReplyMessage(input) {
-  const hands = ["グー", "チョキ", "パー"];
-  const messages = [];
+
   let text = "";
- 
+  let messages = [];
+
   function message(str) {
     return {
       type: "text",
       text: str
     }
   }
-
-  if (hands.indexOf(input) === -1) {
-    text = "グー・チョキ・パーのどれかを入力してね";
-    messages.push(message(text));
-  } else {
-    let user_hand = hands.indexOf(input);
-    let cpu_hand = Math.floor(hands.length * Math.random());
-    text = hands[cpu_hand];
-    messages.push(message(text));
-    let judge_text = "";
-    let judge = (user_hand - cpu_hand + 3) % 3; //じゃんけんの判定
-    if (judge === 0){                           //あいこ
-      judge_text = `aiko`;
-    } else if (judge === 1) {                   //botの勝ち
-      judge_text = `わいの勝ちや！`;
-    } else if (judge === 2) {                   //userの勝ち
-      judge_text = `負けた(;_;)`;
-    }
-    text = judge_text;
+  if(input.type === "follow"){
+　  text = "誰かに追加されました。";
     messages.push(message(text));
   }
-  
+  else if(input.type === "unfollow"){
+  　text = "誰かにブロックされました。";
+  　messages.push(message(text));
+  }
+    
   return messages;
 }
 
@@ -54,7 +41,7 @@ server.post("/", line.middleware(lineConfig), (req, res) => {
 
   for (const event of req.body.events) {
     if (event.type === "message" && event.message.type === "text") {
-      const message = createReplyMessage(event.message.text);
+      const message = createReplyMessage(event.message);
       lineClient.replyMessage(event.replyToken, message);
     }
   }
