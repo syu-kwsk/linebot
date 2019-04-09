e4448981befa81d00f721c203a6460228ca788e1
Your Name <you@example.com>
2019-04-09 10:01:53 +0900

    try

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const lineClient = new line.Client(lineConfig);
 
 function createReplyMessage(input) {
   if(input.source.type === "follow"){
-   input.message.text = "誰かにフォローされました";
+   input.source.type.text = "誰かにフォローされました";
   }
   else if(input.source.type === "unfollow"){
-    input.message.text = "誰かにブロックされました";
+    input.source.type.text = "誰かにブロックされました";
   }
   else{
-    input.message.text = "ブロックしてください";
+    input.source.type.text = "ブロックしてください";
   }
 
 
@@ -35,7 +35,7 @@ server.post("/", line.middleware(lineConfig), (req, res) => {
   res.sendStatus(200);
 
   for (const event of req.body.events) {
-    if (event.type === "message" && event.message.type === "text") {
+    if (event.type === "source" ) {
      const message = createReplyMessage(event);
       lineClient.replyMessage(event.replyToken, message);
     }
