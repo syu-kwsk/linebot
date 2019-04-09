198c65195ff67c563576e2dc3ada793f7e5a1db9
Your Name <you@example.com>
2019-04-09 22:55:01 +0900

    try

--- app_block.js
+++ app_block.js
@@ -36,65 +36,18 @@ server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   res.sendStatus(200);
 
   for (const event of req.body.events) {
-    if (event.source.type === "userId") {
-      const message = createReplyMessage(event);
-      lineClient.pushMessage(event.source.userId, message);
+    if (event.source.type === "user") {
+      const event_message = createReplyMessage(event);
+      lineClient.pushMessage(event.source.userId, event_message);
     }
-    else if(event.source.type == "groupId"){
-      const message = createReplyMessage(event);
-      lineClient.pushMessage(event.source.groupId, message);
+    else if(event.source.type == "group"){
+      const event_message = createReplyMessage(event);
+      lineClient.pushMessage(event.source.groupId, event_message);
     }
-   
-  }
-});
-
-server.listen(process.env.PORT || 8080);
-const path = require("path");
-const express = require("express");
-const line = require("@line/bot-sdk");
-
-const lineConfig = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET
-};
-const lineClient = new line.Client(lineConfig);
-
-function createReplyMessage(input) {
-
-  let message = "";
-
-  if(input.type === "follow"){
-   message = "誰かにフォローされました";
-  }
-  else if(input.type === "unfollow"){
-    message = "誰かにブロックされました";
-  }
-  else{
-    message = "ブロックしてください";
-  }
-
-  return{
-  type: "text",
-  text: message
-  };
-}
-const server = express();
-
-server.use("/images", express.static(path.join(__dirname, "images")));
-
-server.post("/webhook", line.middleware(lineConfig), (req, res) => {
-  // LINEのサーバーに200を返す
-  res.sendStatus(200);
-
-  for (const event of req.body.events) {
-    if (event.source.type === "userId") {
-      const message = createReplyMessage(event);
-      lineClient.pushMessage(event.source.userId, message);
-    }
-    else if(event.source.type == "groupId"){
-      const message = createReplyMessage(event);
-      lineClient.pushMessage(event.source.groupId, message);
-    }
+    // else{
+    //   const event_message = createReplyMessage(event);
+    //   lineClient.pushMessage(event.source.userId, event_message);
+    // }
    
   }
 });
