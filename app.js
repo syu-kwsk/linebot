0f2aaf3cf6cc5ee5a6212ecfc0a007924e47aacd
Your Name <you@example.com>
2019-04-10 16:31:32 +0900

    try

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ function createReplyMessage(input) {
   }
    
 if(input.indexOf("健康") === -1){
-  text = "健康に興味ありませんか？\nBMIを測ります。";
+  text = "健康に興味ありませんか？BMIを測ります。";
   messages.push(make_message(text));
-  text = " 「身長」mと「体重」kgを入力してください。\n単位を半角英数字で忘れないようにしてください！";
+  text = "「身長」mと「体重」kgを入力してください。単位を半角英数字で忘れないようにしてください！";
   messages.push(make_message(text));
 }
 
