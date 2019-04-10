40e4eba9374d059c6e8e04586ecfb530cda4261e
Your Name <you@example.com>
2019-04-10 16:26:42 +0900

    try

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   res.sendStatus(200);
 
   for (const event of req.body.events) {
-    if (event.type === "message" && event.message.type === "text") {
+    if (event.message.type === "text") {
     　const event_message = createReplyMessage(event.message.text);
       lineClient.replyMessage(event.replyToken, event_message);
     }   
