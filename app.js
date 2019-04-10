aff53eaa571e36a4f34766053acb5876acd801ab
Your Name <you@example.com>
2019-04-11 01:34:02 +0900

    try

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function createReplyMessage(input) {
       let weight = parseInt(weigth_data);
 
 
-      BMI = Math.fround(Math.floor( weight*100000 / (length*length) ) /10 );
+      BMI = Math.floor( weight*10000 / (length*length));
 
 
       text = `あなたのBMIは${BMI}です。`;
