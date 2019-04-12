875757a8b4eb89fd5a04f05e641b8e90d6e399d7
Your Name <you@example.com>
2019-04-12 17:23:11 +0900

    try

--- app.js
+++ app.js
@@ -1,116 +1,50 @@
-const path = require("path");
 const express = require("express");
-const line = require("@line/bot-sdk");
+const line    = require("@line/bot-sdk");
+const pg      = require("pg");
+const config  = require("./config.json");
+
+const pool = new pg.Pool(config.db.postgres);
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET
+  channelSecret:      process.env.LINE_CHANNEL_SECRET_KEY
 };
+
 const lineClient = new line.Client(lineConfig);
-
-function createReplyMessage(input) {
-
-  
-  let text;
-  let status = "true";
-  let BMI;
-  const messages = [];
-
-
-  function make_message(str){
-    return{
-      type: "text",
-      text: str
-    };
-  }
-   
-  if(input.indexOf("健康") === -1 && input.indexOf("m") === -1 && input.indexOf("kg") === -1){
-    text = "健康に興味ありませんか？BMIを測ります。";
-    messages.push(make_message(text));
-    text = "「身長」cmと「体重」kgをこの順で入力してください。単位を半角英数字で忘れないようにしてください！";
-    messages.push(make_message(text));
-  }
-  else{
-    if(input.indexOf("cm") > input.indexOf("kg")){
-      text = "!!warning!!\n順番が逆です";
-      messages.push(make_message(text));
-      status = "warning"
-    }
-    if(input.indexOf("cm") === -1 || input.indexOf("kg") === -1){
-      text = "!!warning!!\n要素が抜けています。やり直し！";
-      messages.push(make_message(text));
-      status = "warning"
-    }
-
-    else if(status != "warning"){
-      const m_pos = input.indexOf("cm");
-      const kg_pos = input.indexOf("kg");
-
-      let length_data = input.slice(m_pos - 3, m_pos);
-      let weigth_data = input.slice(kg_pos - 2, kg_pos);
-
-      let length = parseInt(length_data);
-      let weight = parseInt(weigth_data);
-
-        if(isNaN(length) || isNaN(weight)){
-          text = "身長は三桁、体重は二桁以上でお願いします。";
-          messages.push(make_message(text));
-          text = "その範囲にいない方は測定できません";
-          messages.push(make_message(text));
-          status = "not_check";
-        }
-        else if(status == "true"){
-          BMI = Math.fround( weight*10000 / (length*length));
-
-          if(isNaN(BMI)){
-            text = "エラー発生。\nそれは人ですか？"
-            messages.push(make_message(text));
-            status = "error"
-          }
-          
-          else if(status == "true"){
-            text = `あなたのBMIは${BMI}です。`;
-          messages.push(make_message(text));
-
-          if(BMI < 18.5 && BMI > 10){
-            text = "痩せています。\nたくさん食べましょう。";
-            messages.push(make_message(text));
-          }
-          else if(BMI < 22.5 && BMI > 18.5){
-            text = "適正範囲です。\n保ちましょう。";
-            messages.push(make_message(text));
-          }
-          else if(BMI < 40 && BMI > 22.5){
-            text = "太りすぎです。\nさようなら。";
-            messages.push(make_message(text));
-          }
-          else{
-            text = "まじか\nそんなこともあるんですね。";
-            messages.push(make_message(text));
-
-          }
-
-        }        
-      } 
-     } 
-    }
-
-  return messages;
-}
-const server = express();
-
-server.use("/images", express.static(path.join(__dirname, "images")));
+const server     = express();
 
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
-  // LINEのサーバーに200を返す
   res.sendStatus(200);
-
   for (const event of req.body.events) {
-    if (event.type === "message") {
-      const message = createReplyMessage(event.message.text);
-      lineClient.replyMessage(event.replyToken, message);
+    if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
+      if (event.message.text == "履歴") {
+        pool.connect((err, client, done) => {
+          const query = "SELECT * FROM talk WHERE user_id = '"+event.source.userId+"';";
+          console.log("query: " + query);
+          client.query(query, (err, result) => {
+            console.log(result);
+            done();
+            let messages = [];
+            for (const row of result.rows) {
+              messages.push({type: "text", text: row.message});
+            }
+            lineClient.replyMessage(event.replyToken, messages.slice(-5));
+          });
+        });
+      }
+      else {
+        pool.connect((err, client, done) => {
+          const query = "INSERT INTO talk (user_id, message) VALUES ("
+            +"'"+event.source.userId+"', '"+event.message.text+"');";
+          console.log("query: " + query);
+          client.query(query, (err, result) => {
+            done();
+            if (!err) {
+              lineClient.replyMessage(event.replyToken, {type: "text", text: "記録しました。"});
+            }
+          });
+        });
+      }
     }
   }
 });
-
-server.listen(process.env.PORT || 8080);
