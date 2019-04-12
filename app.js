c7b3453945cb59dd3a9ee6a2c201fffa8307331b
Your Name <you@example.com>
2019-04-12 17:56:18 +0900

    try

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
 const express = require("express");
-const path = require("path");
 const line    = require("@line/bot-sdk");
 const pg      = require("pg");
 const config  = require("./config.json");
@@ -9,7 +7,7 @@ const pool = new pg.Pool(config.db.postgres);
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret:      process.env.LINE_CHANNEL_SECRET
+  channelSecret:      process.env.LINE_CHANNEL
 };
 
 const lineClient = new line.Client(lineConfig);
@@ -20,35 +18,35 @@ server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   for (const event of req.body.events) {
     if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
       if (event.message.text == "履歴") {
-        console.log("on rireki");
-        // pool.connect((err, client, done) => {
-        //   const query = "SELECT * FROM talk WHERE user_id = '"+event.source.userId+"';";
-        //   console.log("query: " + query);
-        //   client.query(query, (err, result) => {
-        //     console.log(result);
-        //     done();
-        //     let messages = [];
-        //     for (const row of result.rows) {
-        //       messages.push({type: "text", text: row.message});
-        //     }
-        //     lineClient.replyMessage(event.replyToken, messages.slice(-5));
-        //   });
-        // });
+        pool.connect((err, client, done) => {
+          const query = "SELECT * FROM talk WHERE user_id = '"+event.source.userId+"';";
+          console.log("query: " + query);
+          client.query(query, (err, result) => {
+            console.log(result);
+            done();
+            let messages = [];
+            for (const row of result.rows) {
+              messages.push({type: "text", text: row.message});
+            }
+            lineClient.replyMessage(event.replyToken, messages.slice(-5));
+          });
+        });
       }
       else {
-        console.log("on else");
-        // pool.connect((err, client, done) => {
-        //   const query = "INSERT INTO talk (user_id, message) VALUES ("
-        //     +"'"+event.source.userId+"', '"+event.message.text+"');";
-        //   console.log("query: " + query);
-        //   client.query(query, (err, result) => {
-        //     done();
-        //     if (!err) {
-        //       lineClient.replyMessage(event.replyToken, {type: "text", text: "記録しました。"});
-        //     }
-        //   });
-        // });
+        pool.connect((err, client, done) => {
+          const query = "INSERT INTO talk (user_id, message) VALUES ("
+            +"'"+event.source.userId+"', '"+event.message.text+"');";
+          console.log("query: " + query);
+          client.query(query, (err, result) => {
+            done();
+            if (!err) {
+              lineClient.replyMessage(event.replyToken, {type: "text", text: "記録しました。"});
+            }
+          });
+        });
       }
     }
   }
 });
+
+server.listen(process.env.PORT || 8000);
