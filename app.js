fe319b29ed9294324b84a700c47007909978ac90
Your Name <you@example.com>
2019-04-14 12:30:03 +0900

    try

--- app.js
+++ app.js
@@ -1,52 +1,41 @@
+const path = require("path");
 const express = require("express");
-const line    = require("@line/bot-sdk");
-const pg      = require("pg");
-const config  = require("./config.json");
-
-const pool = new pg.Pool(config.db.postgres);
+const line = require("@line/bot-sdk");
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret:      process.env.LINE_CHANNEL_SECRET
+  channelSecret: process.env.LINE_CHANNEL_SECRET
 };
-
 const lineClient = new line.Client(lineConfig);
-const server     = express();
+
+function createReplyMessage(input) {
+  // 3. 画像を返す
+
+  const appUrl = process.env.HEROKU_APP_URL;
+  return {
+    type: "image",
+    previewImageUrl: `${appUrl}images/takoyaki.png`,
+    originalContentUrl: `${appUrl}images/answer.png`
+  };
+
+  // メッセージオブジェクトに関する公式ドキュメント
+  // https://developers.line.me/ja/reference/messaging-api/#message-objects
+}
+
+const server = express();
+
+server.use("/images", express.static(path.join(__dirname, "images")));
 
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
+  // LINEのサーバーに200を返す
   res.sendStatus(200);
+
   for (const event of req.body.events) {
-    if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
-      if (event.message.text == "履歴") {
-        pool.connect((err, client, done) => {
-          const query = "SELECT * FROM talk WHERE user_id = '"+event.source.userId+"';";
-          console.log("query: " + query);
-          client.query(query, (err, result) => {
-            console.log(result);
-            done();
-            let messages = [];
-            for (const row of result.rows) {
-              messages.push({type: "text", text: row.message});
-            }
-            lineClient.replyMessage(event.replyToken, messages.slice(-5));
-          });
-        });
-      }
-      else {
-        pool.connect((err, client, done) => {
-          const query = "INSERT INTO talk (user_id, message) VALUES ("
-            +"'"+event.source.userId+"', '"+event.message.text+"');";
-          console.log("query: " + query);
-          client.query(query, (err, result) => {
-            done();
-            if (!err) {
-              lineClient.replyMessage(event.replyToken, {type: "text", text: "記録しました。"});
-            }
-          });
-        });
-      }
+    if (event.type === "message" && event.message.type === "text") {
+      const message = createReplyMessage(event.message.text);
+      lineClient.replyMessage(event.replyToken, message);
     }
   }
 });
 
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.PORT || 8080);
