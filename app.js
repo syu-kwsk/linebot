87f83b8cad1e42b4664795289bedf7b4a22e6e07
Your Name <you@example.com>
2019-04-16 13:39:28 +0900

    try

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function createReplyMessage(input) {
     return messages;
 }
 
-server.use("/images", express.static(path.join(__dirname, "images")));
+
 
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   // LINEのサーバーに200を返す
